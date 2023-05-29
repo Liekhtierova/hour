744e9a39be5bfcb9792649e39e5a90c1f83d625d
Liekhtierova Tetiana <liekhtierova@gmail.com>
2023-05-29 18:00:28 +0300

    typeof

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ if (min > 0 && min <= 15) {
     console.log('Третя чверть');
 } else if (min > 45 && min <= 60 || min === 0) {
     console.log('Четверта чверть');
-} else if (min > 60 || min < 0) {
+} else if (min > 60 || min < 0 || typeof min !== 'number') {
     console.log('Значення змінної введено некоректно');
 }
